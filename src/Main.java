ad10cca600c44480b951a8a34c1471aa59b5e313
Vitaly Petukhov <vetal.petuhoff@yandex.ru>
2024-09-30 00:08:16 +0300

    Added file existence check

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+        while (true){
+            int rightPathCount = 1;
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if(fileExists == false || isDirectory == true){
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке");
+                continue;
+            }else{
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + rightPathCount);
+                rightPathCount += 1;
+            }
+        }
     }
 }
